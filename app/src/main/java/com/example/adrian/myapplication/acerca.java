796b3f8c7133c09210098efcbb8f561857c75970
sage2212 <34044178+sage2212@users.noreply.github.com>
2019-04-15 23:00:28 -0600

    prueba

--- app/src/main/java/com/example/adrian/myapplication/acerca.java
+++ app/src/main/java/com/example/adrian/myapplication/acerca.java
@@ -9,6 +9,8 @@ public class acerca extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acerca);
+
+
     }
 
 }
